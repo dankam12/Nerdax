760310c420e070f0f1353021a2101d31f48f8125
DESKTOP-QKT1BNU\daniel <daniel.kamlind@gmail.com>
2016-09-07 21:01:48 +0200

    Added Method RemoveWhiteChars

--- src/Nardax.UnitTests/TruncateStringExtensionsTests.cs
+++ src/Nardax.UnitTests/TruncateStringExtensionsTests.cs
@@ -64,5 +64,14 @@ namespace Nardax.Tests
 
             Assert.AreEqual(expected, result);
         }
+
+        [TestMethod]
+        public void RemoveWhiteChars()
+        {
+            var Teststring = "i\twill  \tsurvive\t";
+            var result = Teststring.RemoveWhiteChars();
+            var expected = "iwillsurvive";
+            Assert.AreEqual(expected, result);
+}
     }
 }
